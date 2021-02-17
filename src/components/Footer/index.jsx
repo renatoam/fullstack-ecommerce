c42cc26d65b5cfba4981b121ad62ecc755a74521
tadeo <salaviawtadeo@gmail.com>
2021-02-17 12:07:30 +0000

    feature: create responsive footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Icon from "../Icon";
+import { useState } from "react";
 
 import {
   faFacebook,
@@ -7,49 +8,85 @@ import {
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import { StyledFooter } from "./styles";
+import { faEnvelope, faMapMarker, faPhone } from "@fortawesome/free-solid-svg-icons";
 
+function Accordion(props) {
+  const { className, children, ...rest } = props;
+  const [display, setDisplay] = useState("");
+  const nameClass = `${className} ${display}`;
+
+  function handleDisplayContent() {
+    const isOpen = display === "open" ? "" : "open";
+    setDisplay(isOpen);
+  }
+
+  return (
+    <section onClick={handleDisplayContent} className={nameClass} {...rest}>
+      {children}
+    </section>
+  )
+}
 
 export default function Footer() {
   return (
     <StyledFooter>
       <section className="up">
         <div className="container">
-          <section>
-            <Icon icon={faFacebook} />
-            <Icon icon={faInstagram} />
-            <Icon icon={faYoutube} />
-          </section>
-          <section>
+          <Accordion className="contact">
+            <h2>Contato</h2>
+            <div>
+              <Icon icon={faFacebook} />
+              <Icon icon={faInstagram} />
+              <Icon icon={faYoutube} />
+            </div>
+            <div>
+              <Icon icon={faMapMarker} />
+              <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
+            </div>
+            <div>
+              <Icon icon={faPhone} />
+              <p>+2 392 3929 210</p>
+            </div>
+            <div>
+              <Icon icon={faEnvelope} />
+              <p>emailaddress@domain.com</p>
+            </div>
+          </Accordion>
+
+          <Accordion className="institutional">
             <h2>Institucional</h2>
             <ul>
               <li>Marca</li>
               <li>Lojas</li>
               <li>Contato</li>
             </ul>
-          </section>
-          <section>
+          </Accordion>
+          <Accordion className="information">
             <h2>Informações</h2>
             <ul>
               <li>Formas de Pagamento</li>
               <li>Trocas e devoluções</li>
               <li>Cuidados com o produto</li>
             </ul>
-          </section>
-          <section>
+          </Accordion>
+          <Accordion className="about">
             <h2>Conheça</h2>
             <ul>
               <li>Franquias e multimarca</li>
               <li>Trabalhe com a gente</li>
               <li>Procon-SP</li>
             </ul>
-          </section>
-          <section>
+          </Accordion>
+          <Accordion className="news">
             <h2>Assine nossa newsletter</h2>
-            <fieldset>
-              <legend>Informe seu melhor e-mail!</legend>
-              <input type="email" name="email" placeholder="E-mail..." />
-            </fieldset>
-          </section>
+            <div className="newsletter">
+              <label htmlFor="email">Informe seu e-mail!</label>
+              <div className="send">
+                <input type="email" name="email" placeholder="E-mail..." />
+                <button>Send</button>
+              </div>
+            </div>
+          </Accordion>
         </div>
       </section>
       <section className="bottom">
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,31 +5,33 @@ import {
   faUser,
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
-import StyledHeader from "./styles";
+import { StyledHeader } from "./styles";
 import Icon from "../Icon";
 import Menu from "../Menu"
 
+
 export default function Header() {
   return (
-    <StyledHeader>
-      <div className="container">
-        <section className="search">
-          <Icon icon={faSearch} />
-          <input type="search" name="search" id="search" placeholder="Search" />
-        </section>
-        <section>
-          <h1>
-            <a href="#">Lójia</a>
-          </h1>
-        </section>
-        <section>
-          <Icon icon={faUser} />
-          <Icon icon={faHeart} />
-          <Icon icon={faShoppingCart} count={2} />
-          <Icon icon={faBars} />
-        </section>
-      </div>
-      <Menu />
-    </StyledHeader>
+    <p>Header</p>
+    // <StyledHeader>
+    //   <div className="container" >
+    //     <section className="search">
+    //       <Icon icon={faSearch} />
+    //       <input type="search" name="search" id="search" placeholder="Search" />
+    //     </section>
+    //     <section>
+    //       <h1>
+    //         <a href="#">Lójia</a>
+    //       </h1>
+    //     </section>
+    //     <section>
+    //       <Icon icon={faUser} />
+    //       <Icon icon={faHeart} />
+    //       <Icon icon={faShoppingCart} count={2} />
+    //       <Icon icon={faBars} />
+    //     </section>
+    //   </div>
+    //   <Menu />
+    // </StyledHeader>
   );
 }
